bf57597160e68e3e471a8577da7d5abd396b7ff4
Noinkin <87577662+Noinkin@users.noreply.github.com>
2026-06-28 11:17:02 +0100

    feat: add exports

---
+++ packages/core/src/index.ts
@@ -0,0 +1,24 @@
+// Client
+export * from './client/client.js';
+
+// Gateway
+export * from './gateway/webhookserver.js';
+
+// REST
+export * from './rest/manager.js';
+export * from './rest/ratelimiter.js';
+
+// Managers
+export * from './managers/commandmanager.js';
+export * from './managers/playermanager.js';
+export * from './managers/servermanager.js';
+export * from './managers/vehiclemanager.js';
+
+// Structures
+export * from './structures/base.js';
+export * from './structures/player.js';
+export * from './structures/server.js';
+export * from './structures/vehicle.js';
+
+// Types
+export * from './types/index.js';
